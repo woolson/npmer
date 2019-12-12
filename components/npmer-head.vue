48886e9468764cb601e6e9e8ce34aea74f5a004f
woolson <woolson.lee@qq.com>
2019-12-12 23:04:49 +0800

    feat: 用户信息样式调整

--- components/npmer-head.vue
+++ components/npmer-head.vue
@@ -30,13 +30,13 @@ div.head
     el-popover(
       v-else
       placement="bottom-end"
-      width="80"
+      width="120"
       trigger="click"
     )
       img(slot="reference" :src="account.avatarUrl")
       ul.head_user-info
         li.el-icon-user-solid
-          a(:href="account.htmlUrl") {{account.login}}
+          a(:href="account.htmlUrl") {{account.name}}
         li.el-icon-star-on
           nuxt-link(to="/user/stared") {{$t('stared')}}
 </template>
